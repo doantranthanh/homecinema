93284d94966add669c0c418610ec646a3cbbf5b2
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-02-09 10:42:55 +0000

    Init HomeCinema.Entities project

---
+++ HomeCinema/HomeCinema.Entities/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeCinema.Entities
+{
+    public class Genre : IEntityBase
+    {
+        public Genre()
+        {
+            Movies = new List<Movie>();
+        }
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public vitual ICollection<Movie> Movies { get; set; }
+    }
+}
