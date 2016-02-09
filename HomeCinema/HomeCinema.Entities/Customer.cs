803451621b1e33f40703a7abb27b24335f9bd60d
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-02-09 11:07:58 +0000

    Adding Customer class

---
+++ HomeCinema/HomeCinema.Entities/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime;
+
+namespace HomeCinema.Entities
+{
+    public class Customer: IEntityBase
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string IdentityCard { get; set; }
+        public Guid UniqueKey { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Mobile { get; set; }
+        public DateTime ReistrationDate { get; set; }
+    }
+}
